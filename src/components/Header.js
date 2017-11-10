7a2b7a4c6bb898840b26a2cf61de7d5e91d28ac2
cor.park <cor.park@doosan.com>
2017-11-10 17:27:47 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router';
+import PropTypes from 'prop-types';
+
+import './Header.css';
+
+const MenuItem = ({active, children, to}) => (
+    <Link to={to} className={`menu-item ${active ? 'active': ''}`}>
+            {children}
+    </Link>
+);
+
+const Header = (props, context) => {
+    const { router } = context;
+    return (
+        <div>
+            <div className="logo">
+                Cor
+            </div>
+            <div className="menu">
+                <MenuItem to={'/'} active={router.isActive('/', true)}>홈</MenuItem>
+                <MenuItem to={'/about'} active={router.isActive('/about')}>소개</MenuItem>
+                <MenuItem to={'/post'} active={router.isActive('/post')}>포스트</MenuItem>
+            </div>
+        </div>
+    );
+};
+
+Header.contextTypes = {
+router: PropTypes.object
+}
+
+export default Header;
